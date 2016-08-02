8084c933e8893f2e65f5b6fd9ad32ab76122be15
prasad akella <prasad@hirecloud.in>
2016-08-02 16:40:48 +0530

    Added test files

---
+++ src/test/java/com/hcp/common/tests/Test1.java
@@ -0,0 +1,12 @@
+package com.hcp.common.tests;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Test1 extends TestCase {
+	 @Test
+	   public void testPrintMessage() {
+	      assertEquals(1,2);
+	   }
+}
